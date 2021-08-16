765323da508c57aeff93372fa975defb9c9ff07a
ahsouza <annibalhsouza@gmail.com>
2021-08-15 22:30:35 -0300

    feat: implement print 'hello world' VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+/// byte static
+static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -10,5 +12,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+///  converting an integer 0xb8000 into a raw pointer primite type
+  let vga_buffer = 0xb8000 as *mut u8;
+/// LOOP: method to write string 'Hello World!' byte and color byte
+  for (i, &byte) in HELLO.iter().enumerate() {
+    unsafe {
+      *vga_buffer.offset(i as isize * 2) = byte;
+      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    }
+  }
+
   loop {}
 }
